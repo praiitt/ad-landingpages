0e0546b5e3bedbfd5fe5cff483813c5aae47a01c
prakashkumarin@gmail.com <prakashkumarin@gmail.com>
2018-04-20 18:22:27 +0530

    added changes

--- php/contact.php
+++ php/contact.php
@@ -43,8 +43,8 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$from = $email;
-	$to = 'info@example.com';  // please change this email id
-	$subject = 'Contact Form : Titan - The best downloaded template ever';
+	$to = 'prakash.rai@absolutedimension.co';  // please change this email id
+	$subject = 'Contact Form : Absolute Dimension Website';
 
 	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
